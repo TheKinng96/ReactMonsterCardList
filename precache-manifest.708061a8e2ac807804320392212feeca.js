1d9d431df85b6afa4ab03458dc2aa70a64d5abff
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-05-19 18:47:22 +0900

    Updates

---
+++ precache-manifest.708061a8e2ac807804320392212feeca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee36d33b5455e9fc25cfa148bb8664d",
+    "url": "/ReactMonsterCardList/index.html"
+  },
+  {
+    "revision": "2d0a33ceb181e04c07fb",
+    "url": "/ReactMonsterCardList/static/css/main.fab0b734.chunk.css"
+  },
+  {
+    "revision": "c17feb71ac951a4e91c6",
+    "url": "/ReactMonsterCardList/static/js/2.5b8ff17e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactMonsterCardList/static/js/2.5b8ff17e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d0a33ceb181e04c07fb",
+    "url": "/ReactMonsterCardList/static/js/main.460ff357.chunk.js"
+  },
+  {
+    "revision": "76428e55e1ef4c57ffb5",
+    "url": "/ReactMonsterCardList/static/js/runtime-main.9670a352.js"
+  }
+]);
